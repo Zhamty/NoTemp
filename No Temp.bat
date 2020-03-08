d70bfa2e11b7e9dfc09aa3f82ba858a5f46200bf
Santiago <33164013+Slaim36@users.noreply.github.com>
2020-03-08 20:43:37 +0000

    Create No Temp.bat

---
+++ No Temp.bat
@@ -0,0 +1,29 @@
+@echo off 
+mode con:cols=1200 lines=700
+chcp 65001
+cls
+title NoTemp
+echo -------------------------------------------------------------------------------------------------------
+echo Hola, bienvenido/a, ¿desea eliminar los archivos temporales de su sistema y hacer que sea más rápido?
+echo Escriba S para sí y N para no, también recuerde las mayúsculas.
+echo Aplicación creada y programada por Slaim36 www.slaim36.com
+echo -------------------------------------------------------------------------------------------------------
+Set/p respuestauno= Respuesta: 
+If %respuestauno%==S (goto s) else goto n
+:s
+echo Un momento.
+echo Un momento..
+echo Un momento...
+pause
+del /s/q %temp% 
+
+wait (3)
+cls
+echo Archivos Temporales eliminados. Presione cualquier tecla para salir.
+pause >nul
+exit 
+:n
+cls
+echo Si no quiere, no le obligamos. Presione cualquier tecla para salir.
+pause >nul
+exit
