f6497e5d7fa5354eafc58bd89aae8946973666a8
Santiago <33164013+Slaim36@users.noreply.github.com>
2020-03-08 21:38:18 +0000

    Update No Temp.bat
    v1.2

--- No Temp.bat
+++ No Temp.bat
@@ -1,15 +1,16 @@
 @echo off 
 mode con:cols=1200 lines=700
 chcp 65001
+:inicio
 cls
 title NoTemp
 echo -------------------------------------------------------------------------------------------------------
 echo Hola, bienvenido/a, ¿desea eliminar los archivos temporales de su sistema y hacer que sea más rápido?
-echo Escriba S para sí y N para no, también recuerde las mayúsculas.
+echo Escriba S para sí y N para no.
 echo Aplicación creada y programada por Slaim36 www.slaim36.com
 echo -------------------------------------------------------------------------------------------------------
 Set/p respuestauno= Respuesta: 
-If %respuestauno%==S (goto s) else goto n
+If %respuestauno%==S (goto s) else If %respuestauno%==s (goto s) else If %respuestauno%==N (goto n) else If %respuestauno%==n (goto n) else goto nn
 :s
 echo Un momento.
 echo Un momento..
@@ -25,3 +26,7 @@ cls
 echo Si no quiere, no le obligamos. Presione cualquier tecla para salir.
 pause >nul
 exit
+:nn
+echo Error, respuesta incorrecta, tiene que responder S o N
+pause
+goto inicio
