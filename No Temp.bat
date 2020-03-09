53b33bc3f516d0b59c2c73b40d3a0a9f369bafa1
Santiago <33164013+Slaim36@users.noreply.github.com>
2020-03-09 18:56:33 +0000

    Update No Temp.bat
    For release v1.3
    Para la actualización v1.3

--- No Temp.bat
+++ No Temp.bat
@@ -1,17 +1,31 @@
 @echo off 
-mode con:cols=1200 lines=700
+mode con:cols=1000 lines=600
 chcp 65001
-:inicio
-cls
 title NoTemp
+:idiomai
+cls
+echo Hola, seleccione tu Idioma de las siguientes opciones (ponga números).
+echo Hello, select your language of the next options (put numbers).
+echo 1. Español
+echo 2. English
+Set/p idioma= Idioma/Language: 
+if %idioma%==1 (goto inicios) else If %idioma%==español (goto inicios) else If %idioma%==Español (goto inicios) else if %idioma%==2 (goto inicion) else If %idioma%==english (goto inicion) else If %idioma%==English (goto inicion) else (goto errori) 
+:errori
+cls
+echo Respuesta incorrecta, presiona cualquier tecla para continuar
+echo Incorrect Answer, press any key to continue
+pause >nul
+goto idiomai
+:inicios
+cls
 echo -------------------------------------------------------------------------------------------------------
 echo Hola, bienvenido/a, ¿desea eliminar los archivos temporales de su sistema y hacer que sea más rápido?
 echo Escriba S para sí y N para no.
-echo Aplicación creada y programada por Slaim36 www.slaim36.com
+echo Aplicación creada y programada por Slaim36 www.slaim36.com (Página en español).
 echo -------------------------------------------------------------------------------------------------------
 Set/p respuestauno= Respuesta: 
-If %respuestauno%==S (goto s) else If %respuestauno%==s (goto s) else If %respuestauno%==N (goto n) else If %respuestauno%==n (goto n) else goto nn
-:s
+If %respuestauno%==S (goto ss) else If %respuestauno%==s (goto ss) else If %respuestauno%==N (goto ns) else If %respuestauno%==n (goto ns) else goto nns
+:ss
 echo Un momento.
 echo Un momento..
 echo Un momento...
@@ -21,12 +35,42 @@ cls
 echo Archivos Temporales eliminados. Presione cualquier tecla para salir.
 pause >nul
 exit 
-:n
+:ns
 cls
 echo Si no quiere, no le obligamos. Presione cualquier tecla para salir.
 pause >nul
 exit
-:nn
-echo Error, respuesta incorrecta, tiene que responder S o N
+:nns
+echo Error, respuesta incorrecta, tiene que responder S o N.
 pause
-goto inicio
+goto inicios
+
+:Inicion
+cls
+echo -------------------------------------------------------------------------------------------------------
+echo Hello, welcome, ¿do you want to delete temporal files and do your system more fast?
+echo Write Y for yes or N for no.
+echo Application created and programed by Slaim36 www.slaim36.com (Page in Spanish).
+echo -------------------------------------------------------------------------------------------------------
+Set/p respuestauno= Answer: 
+If %respuestauno%==Y (goto sn) else If %respuestauno%==y (goto sn) else If %respuestauno%==N (goto nn) else If %respuestauno%==n (goto nn) else goto nnn
+:sn
+echo A moment.
+echo A moment..
+echo A moment...
+pause
+del /s/q %temp% 
+cls
+echo Temporal files deleted. Press any key to leave.
+pause >nul
+exit 
+:nn
+cls
+echo If you don't want, we don't obligate you. Press any key to leave.
+pause >nul
+exit
+:nnn
+echo Error, incorrect answer, you need to answer Y or N.
+echo Press any key to continue
+pause >nul
+goto Inicion
